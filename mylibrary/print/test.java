ee758747f18f93ed34ce87bb5ed1c8bd4f64e6cb
fu908301 <fu908301@gmail.com>
2017-03-20 22:40:07 +0800

    First upload of my library.Construct print to test.

---
+++ mylibrary/print/test.java
@@ -0,0 +1,11 @@
+package bin;
+public class test{
+	public static void main(String[] args){
+		int i = 0;
+		myprint.print("Hello");
+		myprint.println(" this is my number " + i);
+		myprint.println();
+		myprint.println("This is third line");
+	}	
+
+}
