5b3823512c58a5ef69b192709980d8e0595e4c98
CHENG-AN FU <fu908301@gmail.com>
2017-04-26 16:57:00 +0800

    Fighter and Magician completed.

--- homework/HW6/src/myjava/homework/Magician.java
+++ homework/HW6/src/myjava/homework/Magician.java
@@ -1,24 +1,82 @@
 package myjava.homework;
 
+import java.util.*;
+
 public class Magician extends Adventurer implements Skill{
 	
 	public Magician(int hp,int atk,int unique){
 		super(hp,atk,unique);
 	}
 	public int action(){
-		return 0;
+		Scanner scan = new Scanner(System.in);
+		int choice;
+		try{
+			System.out.println("(1) Cold Beam (2) Energy Shield (3) Heal");
+			System.out.println("Action:(By default 1)");
+			choice = scan.nextInt();
+			if(choice == 1){
+				int attack = this.attack_skill();
+				System.out.println("[Cold Beam] : " + attack + " damage");
+				return attack;
+			}
+			else if(choice == 2){
+				int shield = this.defense_skill();
+				if(super.getUnique() <= 0){
+					System.out.println("[Energy Shield] : Shield + " + shield + " points");
+					super.setUnique(super.getUnique() + shield);
+				}
+				else if(super.getUnique() > 0){
+					System.out.println("[Energy Shield] : Shield + 0 points");
+				}
+				return 0;
+			}
+			else if(choice == 3){
+				int heal = this.buff_skill();
+				int over_heal = 40 - super.getHp();
+				if(super.getHp() + heal <= 40){
+					System.out.println("[Heal] : Hp + " + heal + " points");
+					super.setHp(super.getHp() + heal);
+				}
+				else if(super.getHp() + heal > 40){
+					System.out.println("[Heal] : Hp + " + over_heal + " points");
+					super.setHp(40);
+				}
+				return 0;
+			}
+			else{
+				int attack = this.attack_skill();
+				System.out.println("[Cold Beam] : " + attack + " damage");
+				return attack;
+			}
+				
+		}
+		catch(InputMismatchException e){
+			scan.next();
+			int attack = this.attack_skill();
+			System.out.println("[Cold Beam] : " + attack + " damage");
+			return attack;
+		}
 	}
 	
 	public int attack_skill(){
-		return 0;
+		Random ran = new Random();
+		int range = ran.nextInt(11);
+		int damage = super.getAtk() * 4 + range;
+		return damage;
 	}
 	
 	public int defense_skill(){
-		return 0;
+		Random ran = new Random();
+		int range = ran.nextInt(11);
+		int shield = super.getAtk() * 4 + range;
+		return shield;
 	}
 	
 	public int buff_skill(){
-		return 0;
+		Random ran = new Random();
+		int range = ran.nextInt(11);
+		int heal = super.getAtk() * 4 + range;
+		return heal;
 	}
 	
 }
--- homework/HW6/src/myjava/homework/Main.java
+++ homework/HW6/src/myjava/homework/Main.java
@@ -20,15 +20,16 @@ public class Main {
 			int atk = f.action();
 			if(atk != 0)
 				monster_hp -= atk;
-			if(monster_hp > 0)
+			if(monster_hp > 0){
 				f.setHp(f.getHp() - real_damage);
+				System.out.println("[Monster] : " + monster_real_atk + " damage");
+				System.out.println("[Fighter] HP - " + real_damage + " points");
+			}
 			int A_HP = f.getHp();
 			if(A_HP < 0)
 				A_HP = 0;
 			if(monster_hp < 0)
 				monster_hp = 0;
-			System.out.println("[Monster] : " + monster_real_atk + " damage");
-			System.out.println("HP - " + real_damage + " points");
 			System.out.println("---Adventurer---     ---Monster---");
 			System.out.println("HP : " + A_HP + "          HP : " + monster_hp);
 			System.out.println("ATK : " + f.getAtk() + "          ATK : " + monster_atk);
@@ -44,16 +45,71 @@ public class Main {
 			}
 		}
 	}
+	
+	static void magician(){
+		Adventurer f = new Magician(40,20,0);
+		Random ran = new Random();
+		int monster_hp = ran.nextInt(151) + 150;
+		int monster_atk = ran.nextInt(6) + 30;
+		System.out.println("[Monster appeared!]");
+		System.out.println("---Adventurer---     ---Monster---");
+		System.out.println("HP : " + f.getHp() + "                  HP : " + monster_hp);
+		System.out.println("ATK : " + f.getAtk() + "                 ATK : " + monster_atk);
+		System.out.println("ES : " + f.getUnique());
+		System.out.println("---------------       ---------------");
+		while(true){
+			int monster_real_atk = monster_atk + ran.nextInt(11);
+			int atk = f.action();
+			if(atk != 0)
+				monster_hp -= atk;
+			if(monster_hp > 0){
+				if(f.getUnique() <= 0){
+					f.setHp(f.getHp() - monster_real_atk);
+					System.out.println("[Monster] : " + monster_real_atk + " damage");
+					System.out.println("[Energy Shield] : shield - 0 points");
+					System.out.println("[Magician] : HP - " + monster_real_atk + " points");
+				}
+				else if(f.getUnique() > 0){
+					f.setUnique(f.getUnique() - monster_real_atk);
+					System.out.println("[Monster] : " + monster_real_atk + " damage");
+					System.out.println("[Energy Shield] : shield - " + monster_real_atk + " points");
+				}
+					
+			}
+			int A_HP = f.getHp();
+			if(A_HP < 0)
+				A_HP = 0;
+			if(monster_hp < 0)
+				monster_hp = 0;
+			if(f.getUnique() < 0)
+				f.setUnique(0);
+			System.out.println("---Adventurer---     ---Monster---");
+			System.out.println("HP : " + A_HP + "          HP : " + monster_hp);
+			System.out.println("ATK : " + f.getAtk() + "          ATK : " + monster_atk);
+			System.out.println("ES : " + f.getUnique());
+			System.out.println("---------------       ---------------");
+			if(monster_hp <= 0){
+				System.out.println("You win");
+				break;
+			}
+			if(A_HP <= 0){
+				System.out.println("You lose");
+				break;
+			}
+		}
+	}
 	public static void main(String[] args){
 		Scanner scan = new Scanner(System.in);
 		int choice;
+		System.out.println("(1) Fighter (2) Magician (3) Archer");
+		System.out.println("Choose your character (By default 1)");
 		try{
 			choice = scan.nextInt();
 			if(choice == 1){
 				fighter();
 			}
 			else if(choice == 2){
-				System.out.println("This is Magician");
+				magician();
 			}
 			else if(choice == 3){
 				System.out.println("This is Archer");
